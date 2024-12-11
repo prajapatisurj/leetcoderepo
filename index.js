912147bd405005cb46d273d019fe9c14aaa0ade7
Sooraj <sooraj.prajapati@adglobal360.com>
2024-12-11 12:33:08 +0530

    valid anagram

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // duplicate contains
 
-var containsDuplicate = function (nums) {
-  nums.sort((a, b) => a - b);
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] === nums[i - 1]) {
-      return true;
-    }
-  }
-  return false;
-};
+// var containsDuplicate = function (nums) {
+//   nums.sort((a, b) => a - b);
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] === nums[i - 1]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
 
 // let arr=[1,2,3,1]   true
 // let arr=[1,2,3,4]   false
 
 // console.log(containsDuplicate(arr)) //true
 // console.log(containsDuplicate(arr)) // false
+
+// valid anagram
+
+// var validAnagram = function (a, b) {
+//   return a.split("").sort().join("") === b.split("").sort().join("");
+// };
+
+// let a="anagram"
+// let b= "nagaram" true
+
+// let c='rar'
+// let d='car'  false
+
+// console.log(validAnagram(c,d))
